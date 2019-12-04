7df3bdb2e00c2933ddbf60a32cc71d18030e6130
Johnnyr81 <emailjohn81@gmail.com>
2019-12-04 16:17:29 -0600

    update

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+const postRouter = require("./posts/postRouter.js")
+const userRouter = require("./users/userRouter.js")
+
 const server = express();
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.use(postRouter, userRouter);
 
 //custom middleware
 
