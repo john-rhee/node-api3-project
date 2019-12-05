0749d5437f9046afc8c774ea23530f708af30346
Johnnyr81 <emailjohn81@gmail.com>
2019-12-04 21:19:00 -0600

    update

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,88 @@
 const express = require('express');
 
+const postData = require("./postDb.js");
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  postData.get(req.query)
+    .then(stuffs => {
+      res.status(200).json(stuffs);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json({
+        error: "The post information could not be retrieved." 
+      });
+    });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId,  (req, res) => {
+  postData.getById(req.params.id)
+  .then(stuff => {
+      if (stuff) {
+      res.status(200).json(stuff);
+      } else {
+      res.status(404).json({ message: 'ID not found' });
+      }
+  })
+  .catch(error => {
+      console.log(error);
+      res.status(500).json({
+      message: 'Error retrieving the stuff',
+      });
+  });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  postData.remove(req.params.id)
+    .then(removed => {
+        if (removed) {
+        res.status(200).json({ message: 'Delete sucessful' });
+        } else {
+        res.status(404).json({ message: 'ID not found' });
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+        message: 'Error removing the post',
+        });
+    });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id',  (req, res) => {
+  const updatedPost = req.body;
+
+  postData.update(req.params.id, updatedPost)
+    .then(updated => {
+        if (updated) {
+        res.status(200).json(updated);
+        } else {
+        res.status(404).json({ message: 'ID not found' });
+        }
+    })
+    .catch(error => {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+        message: 'Error updating the post',
+        });
+    });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  postData.getById(req.params.id)
+    .then(post => {
+      if (post) {
+        req.post = post
+      } else {
+        res.status(400).json({ errorMessage: 'invalid post ID' })
+      }
+    })
+   next(); 
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,14 +5,21 @@ const userRouter = require("./users/userRouter.js")
 
 const server = express();
 
+server.use(express.json());
+server.use(logger);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-server.use(postRouter, userRouter);
+server.use('/user', userRouter);
+server.use('/post', postRouter);
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} on ${req.originalUrl} timestamp: ${Date.now()}`);
+  next();
+}
 
 module.exports = server;
